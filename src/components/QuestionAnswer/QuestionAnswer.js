ee21a997f268de63adb1759ccc79b91d4ff90f6b
rokydas <droky136@gmail.com>
2020-12-08 23:24:57 +0600

    insert, read and delete done

--- src/components/QuestionAnswer/QuestionAnswer.js
+++ src/components/QuestionAnswer/QuestionAnswer.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './QuestionAnswer.css';
 
-const QuestionAnswer = () => {
+const QuestionAnswer = ({ qa }) => {
+
+    const history = useHistory();
+
+    const deleteQA = (id) => {
+        fetch('http://localhost:5000/delete', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id })
+        })
+            .then(res => res.json())
+            .then(data => {
+                history.replace('/');
+                history.go(0);
+            });
+    }
+
     return (
         <div className="question-answer mb-4">
-            <h4>Q: What is your name?</h4>
-            <h4>A: My name is Roky Das</h4>
+            <h4>Q: {qa.question}</h4>
+            <h4>A: {qa.answer}</h4><br />
+            <button className="btn btn-warning">Update</button> &nbsp;&nbsp;&nbsp;
+            <button onClick={() => deleteQA(qa._id)} className="btn btn-danger">Delete</button><br /><br />
         </div>
     );
 };
--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import QuestionAnswer from '../QuestionAnswer/QuestionAnswer';
 
 const Training = () => {
+
+    const history = useHistory();
+
+    const email = localStorage.getItem('email');
+    const [inputQA, setInputQA] = useState({email});
+
+    const [questionAnswer, setQuestionAnswer] = useState([]);
+    const selectedQuestionAnswer = questionAnswer.filter(qa => qa.email == email);
+
+    const handleBlur = (e) => {
+        const newQA = inputQA;
+        newQA[e.target.name] = e.target.value;
+        setInputQA(newQA);
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/qa')
+            .then(res => res.json())
+            .then(data => setQuestionAnswer(data));
+    }, [])
+
+    const addTraining = (e) => {
+
+        fetch('http://localhost:5000/insert', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputQA)
+        })
+            .then(res => res.json())
+            .then(data => {
+                history.replace('/');
+                history.go(0);
+            });
+        // e.preventDefault();
+    }
+
     return (
         <div className="m-5 text-center">
             <h1 className="text-center m-5">Train your BOT</h1>
-            <QuestionAnswer />
-            <QuestionAnswer />
-            <QuestionAnswer />
+            {
+                selectedQuestionAnswer.map(qa => <QuestionAnswer qa={qa} key={qa._id} />)
+            }
             <form>
-                <input type="text" className="form-control" placeholder="New Question" required/><br />
-                <input type="text" className="form-control" placeholder="New Answer" required/><br />
-                <button className="btn btn-primary">Add Training</button>
+                <input onBlur={handleBlur} name="question" type="text" className="form-control" placeholder="New Question" required /><br />
+                <input onBlur={handleBlur} name="answer" type="text" className="form-control" placeholder="New Answer" required /><br />
+                <button className="btn btn-primary" onClick={addTraining}>Add Training</button>
             </form>
-
         </div>
     );
 };
